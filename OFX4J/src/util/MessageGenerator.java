6e72f878e0ba363fa450c89f08ca89f5a21eec97
LynMQY <mqy0712@gmail.com>
2017-06-29 11:16:52 -0500

    SignUpGen in MessageGenerator

--- OFX4J/src/util/MessageGenerator.java
+++ OFX4J/src/util/MessageGenerator.java
@@ -26,9 +26,6 @@ public class MessageGenerator {
     cfg.setLogTemplateExceptions(false);
     }
     public static void main(String[] args) throws Exception {
-    	SignOnGen();
-    }
-    public static void SignOnGen() throws Exception {
     	UserInfo user1 = new UserInfo();
 		user1.setUsername("teset");
 		user1.setPassword("testpass");
@@ -37,29 +34,55 @@ public class MessageGenerator {
 		testIns.setOrg("Amex");
 		user1.setInstitution(testIns);
 		System.out.println(user1.toMap());
-    	 /* Create a data-model */
-        Map root = user1.toMap();       
-        root.put("hash", UUID.randomUUID().toString().replaceAll("-", ""));
-        String datetime = LocalDateTime.now().format(DateTimeFormatter.ofPattern("yyyyMMddHHmmss"));
-        root.put("currentDtTm", datetime);
-//        root.put("username", "");
-//        root.put("password", "testpass");
-//        root.put("FI_ORG", "testorg");
-//        root.put("FI_ID", "testid");
+		
+   	 	/* Create a data-model */
+        Map root = user1.toMap();
+        String UID = "testUID";
+        
+    	//SignOnGen(root);
+    	//SignUpGen(root, UID);
+    }
+    public static void SignOnGen(Map data) throws Exception {
 
+    	 /* Create a data-model */       
+        //data = root;
+        
+        data.put("hash", UUID.randomUUID().toString().replaceAll("-", ""));
+        String datetime = LocalDateTime.now().format(DateTimeFormatter.ofPattern("yyyyMMddHHmmss"));
+        data.put("currentDtTm", datetime);
+             
         /* Get the template (uses cache internally) */
         Template temp = cfg.getTemplate("SignOn_Template.ftlh");
         //System.out.print(cfg.getTemplateConfigurations());
         /* Merge data-model with template */
         Path path = Paths.get("output");
         Writer out = new OutputStreamWriter(Files.newOutputStream(path));
-        temp.process(root, out);
+        temp.process(data, out);
         out.close();
         // Note: Depending on what `out` is, you may need to call `out.close()`.
         // This is usually the case for file output, but not for servlet output.
     }
-    public static void SignUpGen() throws Exception{
+    
+    //TODO Another input argument for UID if necessary
+    public static void SignUpGen(Map data, String UID) throws Exception{
+
+    	/* Create a data-model */
+        //data = root;
+        
+        data.put("hash", UUID.randomUUID().toString().replaceAll("-", ""));
+        String datetime = LocalDateTime.now().format(DateTimeFormatter.ofPattern("yyyyMMddHHmmss"));
+        data.put("currentDtTm", datetime);
     	
+    	data.put("UID", UID);
+        
+    	/* Get the template (uses cache internally) */
+        Template temp = cfg.getTemplate("SignUp_Template.ftlh");
+        //System.out.print(cfg.getTemplateConfigurations());
+        /* Merge data-model with template */
+        Path path = Paths.get("output");
+        Writer out = new OutputStreamWriter(Files.newOutputStream(path));
+        temp.process(data, out);
+        out.close();
     }
     
 }
