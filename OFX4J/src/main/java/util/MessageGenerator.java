ca2430f19c22ac794f66b0310901064a04dcf3e6
LynMQY <mqy0712@gmail.com>
2017-07-03 15:07:17 -0500

    request body

--- OFX4J/src/main/java/util/MessageGenerator.java
+++ OFX4J/src/main/java/util/MessageGenerator.java
@@ -39,23 +39,6 @@ public class MessageGenerator {
 		cfg.setLogTemplateExceptions(false);
 	}
 
-	public static void main(String[] args) throws Exception {
-		UserInfo user1 = new UserInfo();
-		user1.setUsername("teset");
-		user1.setPassword("testpass");
-		Institution testIns = new Institution();
-		testIns.setID("3101");
-		testIns.setOrg("Amex");
-		user1.setInstitution(testIns);
-		System.out.println(user1.toMap());
-
-		/* Create a data-model */
-		Map<?, ?> root = user1.toMap();
-		String UID = "testUID";
-	
-		// SignOnGen(root);
-		// SignUpGen(root, UID);
-	}
 
 	public static RequestBody SignOnRequestGen(UserInfo user) throws Exception {
 
@@ -79,34 +62,16 @@ public class MessageGenerator {
 		return RequestBody.create(MediaType.parse("application/x-ofx; charset=utf-8"), out.toString());
 	}
 
-	public static void SignOnGen(Map data) throws Exception {
-
-		/* Create a data-model */
-		// data = root;
-
-		data.put("hash", UUID.randomUUID().toString().replaceAll("-", ""));
-		String datetime = LocalDateTime.now().format(DateTimeFormatter.ofPattern("yyyyMMddHHmmss"));
-		data.put("currentDtTm", datetime);
-
-		/* Get the template (uses cache internally) */
-		Template temp = cfg.getTemplate("SignOn_Template.ftlh");
-		// System.out.print(cfg.getTemplateConfigurations());
-		/* Merge data-model with template */
-		Path path = Paths.get("output");
-		Writer out = new OutputStreamWriter(Files.newOutputStream(path));
-		temp.process(data, out);
-		out.close();
-		// Note: Depending on what `out` is, you may need to call `out.close()`.
-		// This is usually the case for file output, but not for servlet output.
-	}
 
 	// TODO Another input argument for UID if necessary
-	public static void SignUpGen(Map data, String UID) throws Exception {
+	public static RequestBody SignUpGen(UserInfo user) throws Exception {
 
 		/* Create a data-model */
 		// data = root;
 
-		data.put("hash", UUID.randomUUID().toString().replaceAll("-", ""));
+		Map<Object, Object> data = user.toMap();
+		String UID = UUID.randomUUID().toString().replaceAll("-", "");
+		data.put("hash", UID);
 		String datetime = LocalDateTime.now().format(DateTimeFormatter.ofPattern("yyyyMMddHHmmss"));
 		data.put("currentDtTm", datetime);
 
@@ -116,10 +81,18 @@ public class MessageGenerator {
 		Template temp = cfg.getTemplate("SignUp_Template.ftlh");
 		// System.out.print(cfg.getTemplateConfigurations());
 		/* Merge data-model with template */
-		Path path = Paths.get("output");
-		Writer out = new OutputStreamWriter(Files.newOutputStream(path));
+//		Path path = Paths.get("output");
+//		Writer out = new OutputStreamWriter(Files.newOutputStream(path));
+//		temp.process(data, out);
+//		out.close();
+		StringWriter out = new StringWriter();
+		// System.out.print(cfg.getTemplateConfigurations());
+		/* Merge data-model with template */
+		// Path path = Paths.get("output");
+		// Writer out = new OutputStreamWriter(Files.newOutputStream(path));
 		temp.process(data, out);
-		out.close();
+		LOGGER.info(out.toString());
+		return RequestBody.create(MediaType.parse("application/x-ofx; charset=utf-8"), out.toString());
 	}
 
 }
